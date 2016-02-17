968d2030784a268f842eaa5c0990baa4691e49ce
pericard <pierre.pericard@ed.univ-lille1.fr>
2016-02-17 13:31:49 +0100

    First try at a build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,27 @@
-#! /usr/bin/bash
+#! /usr/bin/env bash
 
+git submodule update --init --recursive
 
+cd contigsearch
+./compile.sh
+cd ..
+
+mkdir ovgraphbuild/build
+cd ovgraphbuild/build
+cmake ..
+make
+cd ../../
+
+cd sortmerna
+./build.sh
+cd ..
+
+cd sumaclust
+make
+cd ../
+
+ln -sf scripts/* bin/.
+ln -sf sumaclust/sumaclust bin/.
+ln -sf ovgraphbuild/bin/ovgraphbuild bin/.
+ln -sf sortmerna/indexdb_rna bin/.
+ln -sf sortmerna/sortmerna bin/.
