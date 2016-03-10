09f952efc78134e76455f05a3a4e0f037f465d47
pericard <pierre.pericard@ed.univ-lille1.fr>
2016-03-10 16:56:49 +0100

    Now use a tmp dir

--- scripts/sga_assemble.py
+++ scripts/sga_assemble.py
@@ -20,18 +20,43 @@ if __name__ == '__main__':
     parser.add_argument('--sga_bin', metavar='PATH', 
                         type=str, default='sga',
                         help='SGA bin path (by default sga is searched in $PATH)')
+    parser.add_argument('--tmp_dir', metavar='DIR',
+                        type=str, default='/tmp',
+                        help='Tmp dir (/tmp by default)')
     parser.add_argument('--cpu', metavar='INT',
                         type=int, default=3,
                         help='Max number of CPU to use')
     args = parser.parse_args()
     
+    # Get input and output files absolute paths
+    input_filepath = os.path.realpath(args.input_fastq)
+    current_working_dir = os.getcwd()
+    
+    if (args.output_contigs[0] == '/' or args.output_contigs[0] == '~'):
+        # output file was given using an absolute path
+        output_filepath = args.output_contigs
+    else:
+        # output file was given using a relative path
+        output_filepath = current_working_dir + '/' + args.output_contigs
+    
+    # Mkdir tmp dir if need be
+    try:
+        if not os.path.exists(args.tmp_dir):
+            os.makedirs(args.tmp_dir)
+    except OSError:
+        sys.stderr.write("\nERROR: {0} tmp dir cannot be created\n\n".format(args.tmp_dir))
+        raise
+    
+    # Change cwd to tmp dir
+    os.chdir(args.tmp_dir)
+    
     # Preprocessing
     preprocess_output = 'preprocess_output.fq'
     
-    cmd_line = args.sga_bin + ' preprocess -v ' + args.input_fastq
+    cmd_line = args.sga_bin + ' preprocess -v ' + input_filepath
     cmd_line += ' -o ' + preprocess_output
     
-    sys.stdout.write('CMD: {0}\n\n'.format(cmd_line))
+    sys.stdout.write('\nCMD: {0}\n\n'.format(cmd_line))
     subprocess.call(cmd_line, shell=True)
     
     ## Error correction
@@ -40,7 +65,7 @@ if __name__ == '__main__':
     cmd_line += ' -t ' + str(args.cpu) + ' --no-reverse '
     cmd_line += preprocess_output
     
-    sys.stdout.write('CMD: {0}\n'.format(cmd_line))
+    sys.stdout.write('\nCMD: {0}\n\n'.format(cmd_line))
     subprocess.call(cmd_line, shell=True)
     
     # Perform error correction
@@ -49,10 +74,11 @@ if __name__ == '__main__':
     
     cmd_line = args.sga_bin + ' correct -k ' + str(kmer_cutoff)
     cmd_line += ' --discard -x 2 -t ' + str(args.cpu)
+    #~ cmd_line += ' --discard --learn -t ' + str(args.cpu)
     cmd_line += ' -o ' + error_corrected_output_basename + '.fq'
     cmd_line += ' ' + preprocess_output
     
-    sys.stdout.write('CMD: {0}\n\n'.format(cmd_line))
+    sys.stdout.write('\nCMD: {0}\n\n'.format(cmd_line))
     subprocess.call(cmd_line, shell=True)
     
     ## Contig assembly
@@ -61,7 +87,7 @@ if __name__ == '__main__':
     cmd_line += ' -t ' + str(args.cpu)
     cmd_line += ' ' + error_corrected_output_basename + '.fq'
     
-    sys.stdout.write('CMD: {0}\n'.format(cmd_line))
+    sys.stdout.write('\nCMD: {0}\n\n'.format(cmd_line))
     subprocess.call(cmd_line, shell=True)
     
     # Remove exact-match duplicates and reads with low-frequency k-mers
@@ -73,7 +99,7 @@ if __name__ == '__main__':
     cmd_line += '--low-complexity-check -o ' + filtered_output
     cmd_line += ' ' + error_corrected_output_basename + '.fq'
     
-    sys.stdout.write('CMD: {0}\n'.format(cmd_line))
+    sys.stdout.write('\nCMD: {0}\n'.format(cmd_line))
     subprocess.call(cmd_line, shell=True)
     
     # Merge simple, unbranched chains of vertices
@@ -84,7 +110,7 @@ if __name__ == '__main__':
     cmd_line += ' -t ' + str(args.cpu) + ' -o ' + merged_output_basename + '.fa'
     cmd_line += ' ' + filtered_output
     
-    sys.stdout.write('CMD: {0}\n'.format(cmd_line))
+    sys.stdout.write('\nCMD: {0}\n'.format(cmd_line))
     subprocess.call(cmd_line, shell=True)
     
     # Build an index of the merged sequences
@@ -92,7 +118,7 @@ if __name__ == '__main__':
     cmd_line += ' -t ' + str(args.cpu)
     cmd_line += ' ' + merged_output_basename + '.fa'
     
-    sys.stdout.write('CMD: {0}\n'.format(cmd_line))
+    sys.stdout.write('\nCMD: {0}\n\n'.format(cmd_line))
     subprocess.call(cmd_line, shell=True)
     
     # Remove any substrings that were generated from the merge process
@@ -100,7 +126,7 @@ if __name__ == '__main__':
     cmd_line += ' -t ' + str(args.cpu)
     cmd_line += ' ' + merged_output_basename + '.fa'
     
-    sys.stdout.write('CMD: {0}\n'.format(cmd_line))
+    sys.stdout.write('\nCMD: {0}\n\n'.format(cmd_line))
     subprocess.call(cmd_line, shell=True)
     
     # Compute the structure of the string graph
@@ -110,26 +136,29 @@ if __name__ == '__main__':
     cmd_line += ' -t ' + str(args.cpu)
     cmd_line += ' ' + merged_output_basename + '.rmdup.fa'
     
-    sys.stdout.write('CMD: {0}\n'.format(cmd_line))
+    sys.stdout.write('\nCMD: {0}\n'.format(cmd_line))
     subprocess.call(cmd_line, shell=True)
     
     # Perform the contig assembly without bubble popping
     assembly_output_basename = 'assemble'
     
     cmd_line = args.sga_bin + ' assemble -m ' + str(min_overlap)
-    cmd_line += ' -g 0 -d 0.03 -g 0.01 '
-    cmd_line += ' --max-edges 10000 -b 3 -x 10 -l 50 '
+    #~ cmd_line += ' -b 3 -d 0.03 -g 0.01 '
+    cmd_line += ' -b 0'
+    #~ cmd_line += ' -r 10'
+    #~ cmd_line += ' --max-edges 10000 -x 10 -l 100 '
+    cmd_line += ' -x 10 -l 100 '
     cmd_line += ' -o ' + assembly_output_basename
     cmd_line += ' ' + merged_output_basename + '.rmdup.asqg.gz'
     
-    sys.stdout.write('CMD: {0}\n\n'.format(cmd_line))
+    sys.stdout.write('\nCMD: {0}\n\n'.format(cmd_line))
     subprocess.call(cmd_line, shell=True)
     
     ## Final post-processing
     cmd_line = 'cp ' + assembly_output_basename + '-contigs.fa '
-    cmd_line += args.output_contigs
+    cmd_line += output_filepath
     
-    sys.stdout.write('CMD: {0}\n\n'.format(cmd_line))
+    sys.stdout.write('\nCMD: {0}\n\n'.format(cmd_line))
     subprocess.call(cmd_line, shell=True)
     
     exit(0)
