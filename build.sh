775f2fee94e8fead5a6acc461a50ef2d6780adc8
pericard <pierre.pericard@ed.univ-lille1.fr>
2016-05-11 10:16:09 +0200

    Changed contigsearch to componentsearch

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ MATAMOG_DIR=$(pwd)
 
 git submodule update --init --recursive
 
-cd $MATAMOG_DIR/contigsearch
+cd $MATAMOG_DIR/componentsearch
 ./compile.sh
 
 mkdir $MATAMOG_DIR/ovgraphbuild/build
@@ -33,7 +33,7 @@ make
 mkdir $MATAMOG_DIR/bin
 cd $MATAMOG_DIR/bin
 
-ln -sf $MATAMOG_DIR/contigsearch/ContigSearch.jar $MATAMOG_DIR/bin/.
+ln -sf $MATAMOG_DIR/componentsearch/ComponentSearch.jar $MATAMOG_DIR/bin/.
 ln -sf $MATAMOG_DIR/scripts/* $MATAMOG_DIR/bin/.
 ln -sf $MATAMOG_DIR/sumaclust/sumaclust $MATAMOG_DIR/bin/.
 ln -sf $MATAMOG_DIR/ovgraphbuild/bin/ovgraphbuild $MATAMOG_DIR/bin/.
