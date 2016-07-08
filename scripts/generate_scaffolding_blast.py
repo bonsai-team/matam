a4159902adfff0e8f7c0c8bcfd492a1a5d1f047a
pericard <pierre.pericard@ed.univ-lille1.fr>
2016-07-08 16:52:16 +0200

    New algorithm to select blast matches for scaffolding

--- scripts/generate_scaffolding_blast.py
+++ scripts/generate_scaffolding_blast.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+from collections import defaultdict
 
 
 def read_tab_file_handle_sorted(tab_file_handle, factor_index=0):
@@ -30,88 +31,135 @@ def read_tab_file_handle_sorted(tab_file_handle, factor_index=0):
 
 
 if __name__ == '__main__':
-    
+
     # Arguments parsing
     parser = argparse.ArgumentParser(description='')
     # -i / --input_tab
-    parser.add_argument('-i', '--input_tab', 
-                        metavar='INBLAST', 
-                        type=argparse.FileType('r'), 
+    parser.add_argument('-i', '--input_tab',
+                        metavar='INBLAST',
+                        type=argparse.FileType('r'),
                         default='-',
-                        help='Input blast tab file. ' 
+                        help='Input blast tab file. '
                              'Only best matches should be kept')
     # -o / --output_tab
-    parser.add_argument('-o', '--output_tab', 
-                        metavar='OUTBLAST', 
-                        type=argparse.FileType('w'), 
+    parser.add_argument('-o', '--output_tab',
+                        metavar='OUTBLAST',
+                        type=argparse.FileType('w'),
                         default='-',
                         help='Ouput filtered blast tab file')
-    
+
     args = parser.parse_args()
-    
-    
+
     # Variables initialization
     tab_list_list = list()
-    
+
     # Reading blast tab file
     for tab_list in read_tab_file_handle_sorted(args.input_tab, 0):
         query_id = tab_list[0][0]
         tab_list_list.append(sorted(tab_list, key=lambda x: x[1]))
-    
+
+    # Sort by specificity (decreasing match number) and then
+    # by decreasing alignment length. So the first seen contigs
+    # are the long specific ones
     tab_list_list.sort(key=lambda x: (len(x), -int(x[0][3])))
-    
-    #~ print tab_list_list
-    
+
+    #
     kept_references_ids_set = set()
-    
-    for tab_list in tab_list_list:
-        query_id = tab_list[0][0]
-        references_ids_set = frozenset(x[1] for x in tab_list)
-        #~ print references_ids_set
-        
-        references_intersection = references_ids_set & kept_references_ids_set
-        
-        if not len(references_intersection):
-            selected_tab = tab_list[0]
-            kept_references_ids_set.add(selected_tab[1])
-            args.output_tab.write('{0}\n'.format('\t'.join(selected_tab)))
+    fillers_tab_list_list = list()
+    dealt_contigs_num = 0
+    dealt_contigs_id = set()
+
+    # Start iterating
+    while (len(tab_list_list)):
+
+        # Deal with specific contigs
+        tab_list_buffer = list()
+
+        # Purely specific contigs
+        current_conservation_count = len(tab_list_list[0])
+        if current_conservation_count == 1:
+            for tab_list in tab_list_list:
+                if len(tab_list) == 1: # Purely specific contig
+                    tab = tab_list[0]
+                    kept_references_ids_set.add(tab[1]) # Add the matching ref id to the set
+                    # Write the alignment
+                    args.output_tab.write('{0}\n'.format('\t'.join(tab)))
+                    if tab[0] == '1500':
+                        print tab
+                    dealt_contigs_id.add(tab[0])
+                    dealt_contigs_num += 1
+                else:
+                    tab_list_buffer.append(tab_list)
+        # Multiple-choices specific contigs
         else:
+
+            # Count refs in remaining specific contigs alignments
+            ref_count_dict = defaultdict(int)
+            for tab_list in tab_list_list:
+                for tab in tab_list:
+                    ref_count_dict[tab[1]] += 1
+
+            # Get most specific contig so far
+            selected_tab_list = tab_list_list[0]
+            #~ print selected_tab_list
+            tab_list_buffer = tab_list_list[1:]
+
+            selected_tab_list.sort(key=lambda t: (-ref_count_dict[t[1]], t[1]))
+            selected_tab = selected_tab_list[0]
+
+            kept_references_ids_set.add(selected_tab[1]) # Add the matching ref id to the set
+            # Write the alignment
+            args.output_tab.write('{0}\n'.format('\t'.join(selected_tab)))
+            dealt_contigs_id.add(selected_tab[0])
+            dealt_contigs_num += 1
+
+        tab_list_list = tab_list_buffer
+        tab_list_buffer = list()
+
+        # Uses already known fillers
+        fillers_tab_list_buffer = list()
+        for tab_list in fillers_tab_list_list:
+            #
+            references_ids_set = frozenset(x[1] for x in tab_list)
+            references_intersection = references_ids_set & kept_references_ids_set
+            #
+            tab_buffer = list()
             for tab in tab_list:
                 reference_id = tab[1]
                 if reference_id in references_intersection:
                     args.output_tab.write('{0}\n'.format('\t'.join(tab)))
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+                    dealt_contigs_id.add(tab[0])
+                else:
+                    # Store the alignments not already used
+                    tab_buffer.append(tab)
+            if tab_buffer:
+                fillers_tab_list_buffer.append(tab_buffer)
+            else:
+                dealt_contigs_num += 1
+        fillers_tab_list_list = fillers_tab_list_buffer
+        fillers_tab_list_buffer = list()
+
+        # Deal with new fillers
+        for tab_list in tab_list_list:
+            #
+            references_ids_set = frozenset(x[1] for x in tab_list)
+            references_intersection = references_ids_set & kept_references_ids_set
+            #
+            if len(references_intersection):  # So fillers
+                tab_buffer = list()
+                for tab in tab_list:
+                    reference_id = tab[1]
+                    if reference_id in references_intersection:
+                        args.output_tab.write('{0}\n'.format('\t'.join(tab)))
+                        dealt_contigs_id.add(tab[0])
+                    else:
+                        # Store the alignments not already used
+                        tab_buffer.append(tab)
+                if tab_buffer:
+                    fillers_tab_list_list.append(tab_buffer)
+                else:
+                    dealt_contigs_num += 1
+            else: # Still specific
+                tab_list_buffer.append(tab_list)
+        tab_list_list = tab_list_buffer
+        tab_list_buffer = list()
