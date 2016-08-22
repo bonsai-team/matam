2d81924624e11a85342005694626d0bb30f71f41
pericard <pierre.pericard@ed.univ-lille1.fr>
2016-08-22 10:23:39 +0200

    Improve readability

--- get_default_db.sh
+++ get_default_db.sh
@@ -5,10 +5,12 @@ cd $MATAMOG_DIR
 
 git lfs pull
 
-echo "\n-- Extracting default ref db --"
+echo
+echo "-- Extracting default ref db --"
 cd $MATAMOG_DIR/db
 tar jxvf SILVA_123_SSURef_rdNs_NR95.tar.bz2
 
-echo "\n-- Indexing default ref db --"
+echo
+echo "-- Indexing default ref db --"
 cd $MATAMOG_DIR
 $MATAMOG_DIR/scripts/index_ref_db.py -v
