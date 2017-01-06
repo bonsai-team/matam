8cacc3e1c72c56d6a05d7f49063377a42c8f9bec
Pericard Pierre <pierre.pericard@gmail.com>
2017-01-06 15:51:03 +0100

    Corrected potential bug

--- scripts/fastq_name_filter.py
+++ scripts/fastq_name_filter.py
@@ -56,7 +56,7 @@ def read_fastq_file_handle(fastq_file_handle):
         if count % 4 == 1:
             if header:
                 yield header, seq, qual
-            header = line[1:]
+            header = line[1:].split()[0]
         elif count % 4 == 2:
             seq = line
         elif count % 4 == 0:
