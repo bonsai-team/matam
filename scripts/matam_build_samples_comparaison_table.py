c3adf4092035665d135655ef8cd198358bc8dd57
lcouderc <loic.couderc@univ-lille1.fr>
2017-10-02 14:02:59 +0200

    Produce the contingency table #41

--- scripts/matam_build_samples_comparaison_table.py
+++ scripts/matam_build_samples_comparaison_table.py
@@ -23,49 +23,91 @@ class SampleCollection():
 
     def __init__(self, samples_path):
         self.float_precision = 4
-        self.samples = samples_path
+        self.samples_path = samples_path
+        #keep the same order to generate the tables
+        self.samples_id = [k for (k,v) in samples_path.items()] #no keys method for ordereddict
         self.comparaison_table = [('Taxonomy', 'SampleID', 'SequenceID', 'Abundance', 'Normalized_Abundance(%)')]
+        self.contingency_table = [['Taxonomy/Samples', *self.samples_id]]
 
         if not samples_path:
             logger.fatal('The sample collection is empty')
             sys.exit('Empty collection')
         
-        self.check_path_validity()
-        self.build_tables()
+        self._check_path_validity()
+        #contingency table is build from the comparaison table
+        self._build_comparaison_table()
+        self._build_contingency_table()
 
 
     def write_comparaison_table(self, out_handler):
-        for row in self.comparaison_table:
+        self._write_table(self.comparaison_table, out_handler)
+
+
+    def write_contingency_table(self, out_handler):
+        self._write_table(self.contingency_table, out_handler)
+
+    def _write_table(self, table, out_handler):
+        for row in table:
             str_row = [str(v) for v in row]
             print('\t'.join(str_row), file=out_handler)
 
+    def _groupby(self, table, operator):
+        return itertools.groupby(sorted(table,key=operator), operator)
 
-    def build_tables(self):
-        for sample_id, (fasta_path, rdp_path) in self.samples.items():
+
+    def _build_comparaison_table(self):
+        taxonomic_col = operator.itemgetter(0)
+        sample_col = operator.itemgetter(1)
+
+        for sample_id, (fasta_path, rdp_path) in self.samples_path.items():
             sample_comp_table = []
             abundance_by_sequence = get_abundance_from_fasta(fasta_path)
-            total_abd = sum(abundance_by_sequence.values())
+            total_abundance = sum(abundance_by_sequence.values())
             for rdp_line in read_rpd_file(rdp_path):
                 sequence_id = rdp_line[0]
                 abundance = abundance_by_sequence[sequence_id]
-                normalized_abundance = round(abundance / total_abd * 100, self.float_precision)
+                normalized_abundance = round(abundance / total_abundance * 100, self.float_precision)
                 taxonomy = ';'.join(get_lineage(rdp_line))
                 row = [taxonomy, sample_id, sequence_id, abundance, normalized_abundance]
                 sample_comp_table.append(row)
 
-            sample_comp_table = sorted(sample_comp_table,key=operator.itemgetter(0)) #sort sample by taxo
+            sample_comp_table = sorted(sample_comp_table, key=taxonomic_col)
             self.comparaison_table.extend(sample_comp_table)
 
 
-    def check_path_validity(self):
-        for sample_id, (fasta_path, rdp_path) in self.samples.items():
+    def _build_contingency_table(self):
+        taxonomic_col = operator.itemgetter(0)
+        sample_col = operator.itemgetter(1)
+        unpivoted_contingence_table = []
+
+        # Build long format contingency table
+        for sample, sample_group in self._groupby(self.comparaison_table[1:],sample_col):
+            sample_group_list = list(sample_group)
+            total_abundance = sum([ row[3] for row in sample_group_list ])
+
+            for taxonomy, taxonomic_group in self._groupby(sample_group_list, taxonomic_col):
+                taxonomic_group_list = list(taxonomic_group)
+                taxonomic_abundance = sum([ row[3] for row in taxonomic_group_list ])
+                normalized_taxo_abd = round(taxonomic_abundance / total_abundance * 100, self.float_precision)
+                row = [taxonomy, sample, normalized_taxo_abd]
+                unpivoted_contingence_table.append(row)
+
+        # Pivot the table to wide format
+        for taxonomy, taxonomic_group in self._groupby(unpivoted_contingence_table, taxonomic_col):
+            row = [taxonomy]
+            abd_by_sample = dict([(row[1], row[2]) for row in taxonomic_group])
+            row.extend( [abd_by_sample.get(sample, None) for sample in self.samples_id])
+            self.contingency_table.append(row)
+
+
+    def _check_path_validity(self):
+        for sample_id, (fasta_path, rdp_path) in self.samples_path.items():
             if not os.path.isfile(fasta_path):
                 logger.fatal('Invalid fasta path (sample: %s):%s' % (sample_id, fasta_path))
                 sys.exit('Invalid file')
             if not os.path.isfile(rdp_path):
                 logger.fatal('Invalid RDP path (sample: %s):%s' % (sample_id, rdp_path))
                 sys.exit('Invalid file')
-
 
 
 def retrieve_samples_path(listing_file):
@@ -80,7 +122,7 @@ def retrieve_samples_path(listing_file):
     with open(listing_file, 'r') as lst_handler:
         for line_number, line in enumerate(lst_handler):
             line = line.strip()
-            if not line: break
+            if not line: continue
             arr_line = line.split('\t')
             arr_line = [v.strip() for v in arr_line]
             if len(arr_line) != 3:
@@ -96,26 +138,42 @@ def retrieve_samples_path(listing_file):
 
 if __name__ == '__main__':
 
+    logging.basicConfig(level=logging.INFO)
     # Arguments parsing
     parser = argparse.ArgumentParser(description='')
 
-    parser.add_argument('--samples_file',
+    parser.add_argument('-s', '--samples_file',
                         type=argparse.FileType('r'),
                         help="A tabulated file with one sample by row." \
-                         "The first column contains the sample id (must be uniq)" \
-                         "The second column contains the fasta path" \
+                         "The first column contains the sample id (must be unique)" \
+                         "The second column contains the fasta path. The abundances must be present into this file." \
                          "The third, the rdp path",
                          required=True)
 
-    parser.add_argument('-o', '--ouput_table',
+    parser.add_argument('-t', '--ouput_comparaison_table',
+                        type=argparse.FileType('w'),
+                        help='Output a table with the abundance for each sequence',
+                        required=True)
+
+    parser.add_argument('-c', '--ouput_contingency_table',
                         type=argparse.FileType('w'),
                         default='-',
-                        help='Output table file')
+                        help='Output a contingency table (taxonomy vs samples)',
+                        required=True)
 
     args = parser.parse_args()
 
+
+    logger.info("Parsing the sample list")
     lst_path = os.path.abspath(args.samples_file.name)
     samples_path = retrieve_samples_path(lst_path)
+
+    logger.info("Build the tables")
     sample_collection = SampleCollection(samples_path)
-    sample_collection.write_comparaison_table(args.ouput_table)
+
+    logger.info("Write the tables")
+    sample_collection.write_comparaison_table(args.ouput_comparaison_table)
+    sample_collection.write_contingency_table(args.ouput_contingency_table)
+
+    logger.info("Done")
     
