2431e6f722806709a0e3f8fb9f7c880353badd31
lcouderc <loic.couderc@univ-lille1.fr>
2017-10-05 15:54:40 +0200

    Plot the coverage comparaison for each reference in the dataframe

--- scripts/evaluation/coverage/plot_coverage_comparaison.R
+++ scripts/evaluation/coverage/plot_coverage_comparaison.R
@@ -11,30 +11,12 @@ option_list = list(
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
-
-REFERENCES <-
-  c(
-    'EU286989.1.1503',
-    'AM980830.1.1445',
-    'JQMC01000035.331.1802',
-    'JF219847.1.1310',
-    'KM356606.1.1440',
-    'JQ427863.1.1441',
-    'AJ412686.1.1278',
-    'KC286759.1.1477',
-    'KF712645.1.1504',
-    'AY221072.1.1451',
-    'JN860392.1.1464',
-    'AJ318187.1.1488',
-    'GU118209.1.1413'
-  )
-
 print("Parse input table")
 coverage <- read_delim(opt$intable, "\t", escape_double = FALSE, col_types = cols(sampling = col_factor(levels = c("before", "after"))), trim_ws = TRUE)
 
 print("Plot results")
 pdf(opt$outpdf)
-for ( reference in REFERENCES ) {
+for ( reference in sort(unique(coverage$reference)) ) {
   subset_cov <- coverage[coverage$reference == reference, ]
   gplot <- ggplot(subset_cov, aes(x=position, y=coverage, color=sampling))  + 
     geom_line() + 
