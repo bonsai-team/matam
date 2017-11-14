c5a18bf2eeca0185b5b0df5bff71dc29eff42bf6
lcouderc <loic.couderc@univ-lille1.fr>
2017-11-14 15:08:29 +0100

    Use the default confidence cutoff of rdp: 0.8

--- scripts/rdp.py
+++ scripts/rdp.py
@@ -7,7 +7,7 @@ import sys
 
 logger = logging.getLogger(__name__)
 
-def run_rdp_classifier(rdp_exe, in_fasta, out_classification_file, cutoff=0.9, gene='16srrna'):
+def run_rdp_classifier(rdp_exe, in_fasta, out_classification_file, cutoff=0.8, gene='16srrna'):
 
     parameters = { 'fa': in_fasta, 'out': out_classification_file, 'cutoff': cutoff, 'gene': gene }
     cmd_line = '{rdp_exe} classify -c {cutoff} -f allrank -g {gene} -o {out} {fa}'.format(rdp_exe=rdp_exe, **parameters)
