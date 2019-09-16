ad9558c4b43504c95c9d2abe00c4906fdab1dc18
Loic Couderc <loic.couderc@univ-lille.fr>
2019-09-16 09:06:04 +0200

    Do not use travis anymore as it's not enough significant: our functional test is interrupted before it ends as it takes too much time to complete.

--- travis_download_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget --quiet http://bioinfo.lifl.fr/matam/SILVA_128_SSURef_NR95_indexed_max_mem_3G.tar.xz{.md5,}
-md5sum -c SILVA_128_SSURef_NR95_indexed_max_mem_3G.tar.xz.md5
-tar Jxvf SILVA_128_SSURef_NR95_indexed_max_mem_3G.tar.xz
